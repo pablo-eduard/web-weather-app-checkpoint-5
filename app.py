e01a0e5af8bfdd65c51f7f13332a72d298f72dfd
pablo-eduard <pablitogoncalves2008@gmail.com>
2026-06-07 23:38:40 -0300

    Commit Final

--- app.py
+++ app.py
@@ -2,7 +2,8 @@ from flask import Flask, render_template, request
 import requests
 
 app = Flask(__name__)
-API_KEY = "82c49aa211d8c8177b647b1eaa26b300" # Cole a chave obtida no site OpenWeatherMap
+# Sua chave já tá configurada aqui
+API_KEY = "82c49aa211d8c8177b647b1eaa26b300" 
 
 @app.route("/", methods=["GET", "POST"])
 def index():
